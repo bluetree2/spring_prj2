f5607fc25d7f77ecf7c072f3aafe4ff0bc1ef09a
bluetree2 <kmsid77@gmail.com>
2025-06-25 11:14:17 +0900

    초기 테이블 생성

---
+++ sql/prj2.sql
@@ -0,0 +1,30 @@
+USE prj2;
+
+Create table member
+(
+    id         VARCHAR(26)    NOT NULL,
+    password   VARCHAR(255)   NOT NULL,
+    nickname   VARCHAR(26)    NOT NULL,
+    info       VARCHAR(10000) Null,
+    created_dt datetime       NOT NULL DEFAULT NOW(),
+    updated_dt datetime       NOT NULL DEFAULT NOW(),
+    CONSTRAINT pk_member PRIMARY KEY (id)
+);
+
+CREATE TABLE todolist
+(
+    id           VARCHAR(26)   NOT NULL,
+    todo_title   VARCHAR(255)  NOT NULL,
+    todo_content VARCHAR(8000) NULL,
+    started_dt   datetime      NOT NULL DEFAULT (NOW() + INTERVAL 1 HOUR),
+    fnished_dt   datetime      NOT NULL DEFAULT (NOW() + INTERVAL 2 HOUR),
+    conpleted    boolean       NOT NULL DEFAULT false,
+    created_dt   datetime      NOT NULL DEFAULT NOW(),
+    updated_dt   datetime      NOT NULL DEFAULT NOW(),
+    CONSTRAINT pk_todolist PRIMARY KEY (id)
+
+);
+
+Alter TABLE todolist
+    ADD FOREIGN KEY (id) REFERENCES member (id);
+
